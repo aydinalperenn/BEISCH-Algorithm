715ff1f288aadbcc67ba4ddda4b24ac38b76b12b
Alperen Aydın <aa.aydinalperen@gmail.com>
2024-02-28 22:19:26 +0300

    Project Files Added

---
+++ CENG307_#201180014#_HW1/CENG307_#201180014#_HW1/FrmMain.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CENG307__201180014__HW1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgridBeisch = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mTxtKeyNum = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.mTxtTableRow = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgridBeisch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-8, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 140);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(661, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 61);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "File Organization";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(184, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 61);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alperen Aydın";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgridBeisch);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(968, 578);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BEISCH";
+            // 
+            // dgridBeisch
+            // 
+            this.dgridBeisch.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgridBeisch.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgridBeisch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgridBeisch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgridBeisch.Location = new System.Drawing.Point(3, 42);
+            this.dgridBeisch.Name = "dgridBeisch";
+            this.dgridBeisch.Size = new System.Drawing.Size(962, 533);
+            this.dgridBeisch.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 746);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Anahtar Sayısını Girin:";
+            // 
+            // mTxtKeyNum
+            // 
+            this.mTxtKeyNum.BackColor = System.Drawing.Color.Silver;
+            this.mTxtKeyNum.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mTxtKeyNum.Location = new System.Drawing.Point(274, 743);
+            this.mTxtKeyNum.Mask = "0000";
+            this.mTxtKeyNum.Name = "mTxtKeyNum";
+            this.mTxtKeyNum.Size = new System.Drawing.Size(61, 37);
+            this.mTxtKeyNum.TabIndex = 5;
+            this.mTxtKeyNum.ValidatingType = typeof(int);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(32, 775);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "(Maksimum 1000)";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnCompare.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCompare.Location = new System.Drawing.Point(423, 775);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(291, 70);
+            this.btnCompare.TabIndex = 9;
+            this.btnCompare.Text = "Algoritmaları Karşılaştır";
+            this.btnCompare.UseVisualStyleBackColor = false;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // mTxtTableRow
+            // 
+            this.mTxtTableRow.BackColor = System.Drawing.Color.Silver;
+            this.mTxtTableRow.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mTxtTableRow.Location = new System.Drawing.Point(274, 843);
+            this.mTxtTableRow.Mask = "0000";
+            this.mTxtTableRow.Name = "mTxtTableRow";
+            this.mTxtTableRow.Size = new System.Drawing.Size(61, 37);
+            this.mTxtTableRow.TabIndex = 7;
+            this.mTxtTableRow.ValidatingType = typeof(int);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(8, 834);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Oluşturulacak Tablonun";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(68, 863);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Satır Sayısı:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(782, 767);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 96);
+            this.listBox1.TabIndex = 11;
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.btnCompare;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(993, 911);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mTxtTableRow);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mTxtKeyNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alperen Aydın (BEISCH Algorithm)";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgridBeisch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox mTxtKeyNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.MaskedTextBox mTxtTableRow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgridBeisch;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CENG307_#201180014#_HW1/CENG307_#201180014#_HW1/FrmMain.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CENG307__201180014__HW1
+{
+    public partial class FrmMain : Form
+    {
+        int keyNum;
+        int tableNum;
+        int[] keys;
+        int mod;
+
+        bool isMadeTable = false;
+
+        // birinci int kısmı index
+        // ikinci list kısmına sırasıyla Record, Link ve Probe gelecek
+
+        Dictionary<int, List<int>> beischTable;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BeischAlgorithm()
+        {
+         
+            if(isMadeTable == false)
+            {
+                BeischAlgorithm();
+                return;
+            }
+            else
+            {
+                foreach (int key in keys)
+                {
+                    int probe = 0;
+                    int index = key % mod;
+                    bool lookForLast = true;
+
+
+                    while (true)
+                    {
+                        probe++;
+
+                        if (beischTable[index][0] == -1)
+                        {
+                            beischTable[index][0] = key;
+                            beischTable[index][2] = probe;
+                            break;
+                        }
+                        else
+                        {
+
+                            int linkOfBeforeNode = beischTable[index][1];
+
+                            if (lookForLast)
+                            {
+                                // son değere bakacak
+                                for (int indexLast = beischTable.Keys.Count - 1; indexLast >= 0; indexLast--)
+                                {
+                                    probe++;
+
+                                    if (beischTable[indexLast][0] == -1)
+                                    {
+                                        beischTable[indexLast][0] = key;
+                                        beischTable[indexLast][1] = linkOfBeforeNode;
+                                        beischTable[indexLast][2] = probe;
+                                        beischTable[index][1] = indexLast;
+                                        lookForLast = !lookForLast;
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                // ilk değere bakacak
+                                for (int indexFirst = 0; indexFirst < beischTable.Keys.Count; indexFirst++)
+                                {
+                                    probe++;
+
+                                    if (beischTable[indexFirst][0] == -1)
+                                    {
+                                        beischTable[indexFirst][0] = key;
+                                        beischTable[indexFirst][1] = linkOfBeforeNode;
+                                        beischTable[indexFirst][2] = probe;
+                                        beischTable[index][1] = indexFirst;
+                                        lookForLast = !lookForLast;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+
+                }
+
+                WriteToBeischDatagrid();
+            }
+
+            
+
+        }
+        
+
+        private void WriteToBeischDatagrid()
+        {
+            dgridBeisch.Columns.Clear();
+            dgridBeisch.Rows.Clear();
+
+            // DataGridView'e sütunları ekle
+            dgridBeisch.Columns.Add("IndexColumn", "Index");
+            dgridBeisch.Columns.Add("RecordColumn", "Record");
+            dgridBeisch.Columns.Add("LinkColumn", "Link");
+            dgridBeisch.Columns.Add("ProbeColumn", "Probe");
+
+
+            foreach (var kvp in beischTable)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+
+                DataGridViewCell keyCell = new DataGridViewTextBoxCell();
+                keyCell.Value = kvp.Key;
+                row.Cells.Add(keyCell);
+
+                for (int i = 0; i < kvp.Value.Count; i++)
+                {
+                    DataGridViewCell valueCell = new DataGridViewTextBoxCell();
+
+                    if (kvp.Value[i] != -1)
+                    {
+                        valueCell.Value = kvp.Value[i];
+
+                    }
+                    else
+                    {
+                        valueCell.Value = null;
+
+                    }
+
+                    row.Cells.Add(valueCell);
+                }
+
+                dgridBeisch.Rows.Add(row);
+            }
+        }
+
+
+
+        private void MakeTables()
+        {
+            beischTable = new Dictionary<int, List<int>>();
+            for(int i = 0; i < tableNum; i++)
+            { 
+                beischTable.Add(i, new List<int> { -1, -1, -1 });
+            }
+
+            isMadeTable = true;
+
+        }
+
+        private void FindModValue()
+        {
+            listBox1.Items.Clear();
+
+            int num = keyNum+1;
+            while (true)
+            {
+                int prime = 0;
+
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        prime++;
+                        break;
+
+                    }
+                
+                }
+
+                if (prime == 0)
+                {
+                    mod = num;
+                    break;
+                }
+
+                num++;
+            }
+            listBox1.Items.Add("Mod: " + mod);
+
+        }
+        private void InstantiateKeys(int num)
+        {
+
+            keys = new int[num];
+            Random random = new Random();
+
+            for (int i = 0; i < num; i++)
+            {
+                keys[i] = random.Next(1, 1001);
+            }
+
+        }
+
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+
+
+            if (mTxtKeyNum.Text.Length == 0)
+            {
+                MessageBox.Show("Anahtar sayısı boş bırakılamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (mTxtTableRow.Text.Length == 0)
+                {
+                    MessageBox.Show("Satır sayısı boş bırakılamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (Convert.ToInt32(mTxtKeyNum.Text) > 1000)
+                    {
+                        MessageBox.Show("En fazla 1000 rastgele anahtar olmalı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(mTxtKeyNum.Text) >= Convert.ToInt32(mTxtTableRow.Text))
+                        {
+                            MessageBox.Show("Satır sayısı anahtar sayısından küçük veya aynı olamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            keyNum = Convert.ToInt32(mTxtKeyNum.Text);
+                            tableNum = Convert.ToInt32(mTxtTableRow.Text);
+                            InstantiateKeys(keyNum);
+                            MakeTables();
+                            FindModValue();
+                            BeischAlgorithm();
+
+                        }
+
+
+                    }
+
+                }
+
+            }
+
+
+
+            Cursor = Cursors.Default;
+        }
+
+    }
+}
